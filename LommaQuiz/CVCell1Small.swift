e1d386beba09813de9261efe302e98ac89d42c3b
jörgen persson <jorgen_raby@icloud.com>
2022-09-11 00:27:39 +0200

    test

---
+++ LommaQuiz/CVCell1Small.swift
@@ -0,0 +1,13 @@
+//
+//  CVCell1Small.swift
+//  LommaQuiz
+//
+//  Created by jörgen persson on 2022-09-09.
+//
+
+import UIKit
+
+class CVCell1Small: UICollectionViewCell {
+    
+    @IBOutlet weak var bingoChart1Label: UILabel!
+}
