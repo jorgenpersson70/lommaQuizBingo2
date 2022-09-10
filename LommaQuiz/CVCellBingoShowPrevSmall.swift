e0ecf822aaf5808a20ca65473aa14ef44551df97
jörgen persson <jorgen_raby@icloud.com>
2022-09-11 00:28:12 +0200

    test

---
+++ LommaQuiz/CVCellBingoShowPrevSmall.swift
@@ -0,0 +1,14 @@
+//
+//  CVCellBingoShowPrevSmall.swift
+//  LommaQuiz
+//
+//  Created by jörgen persson on 2022-09-09.
+//
+
+import UIKit
+
+class CVCellBingoShowPrevSmall: UICollectionViewCell {
+    
+    @IBOutlet weak var showPrevValues: UILabel!
+    //   @IBOutlet weak var showPrevValues: UILabel!
+}
